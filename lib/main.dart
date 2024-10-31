7345fcd877e12733686669621725323efc887d6d
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2024-10-31 18:46:55 +0300

    Preparing Work Manager Service

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notifications/home_screen.dart';
 import 'package:flutter_notifications/services/local_notification_service.dart';
+import 'package:flutter_notifications/services/work_manager_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await LocalNotificationService.init();
+  await Future.wait([
+    LocalNotificationService.init(),
+    WorkManagerService().init(),
+  ]);
   runApp(const MyApp());
 }
 
